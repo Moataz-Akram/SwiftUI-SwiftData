cc5fb4d9ac690a62d202513c0faa34e671f12f5d
Moataz-Akram <moataz.akram@vodafone.com>
2024-01-31 22:39:34 +0200

    Initial Commit

---
+++ SwiftUI+SwiftData/SwiftUI_SwiftDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SwiftDataApp.swift
+//  SwiftUI+SwiftData
+//
+//  Created by Moataz Akram on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SwiftDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
